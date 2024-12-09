4bfe8fe84a8031c8316537c4b9d1c719559df1b7
Stephen Carder <stephen@stafngrimr.com>
2024-12-09 16:36:35 +0000

    initial notes js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-// Game logic.
+/*
+Store the gameboard as an arr inside of an obj
+Players are also stored as obj
+Object to control the flow of the game
 
+NO GLOBAL CODE.
+*/
+
+const gameBoard = (function () {
+    const rowA = [1, 2, 3];
+    const rowB = [1, 2, 3];
+    const rowC = [1, 2, 3];
+})();
 
 console.log("Hey buddy!")
 
-let a = 4;
-let b = 4;
-
-let answer = a * b;
-
-
-console.log(answer);
