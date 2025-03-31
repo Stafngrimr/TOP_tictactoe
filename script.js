f81b6c19a7baf0051490c7d7cfc949c155af2927
Stephen Carder <stephen@stafngrimr.com>
2025-03-31 16:49:38 +0100

    little changes here and there..

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ Store the gameboard as an arr inside of an obj
 Players are also stored as obj
 Object to control the flow of the game
 
-NO GLOBAL CODE. That's the rule
+NO GLOBAL CODE.
 */
 
 const gameBoard = (function () {
@@ -20,6 +20,7 @@ function createGreeting(greeting) {
 }
 
 const theHello = createGreeting("Hello there");
+
 const theHey = createGreeting("Hey, little");
 const theRude = createGreeting("Fuck you");
 
@@ -27,3 +28,12 @@ console.log(theHello("Stephen"));
 console.log(theHey("Penny"));
 console.log(theRude("Zoe"));
 
+
+function setTheme() {
+    const root = document.documentElement;
+    const newTheme = (root.className === 'green') ? 'blue' : 'green';
+    root.className = newTheme;
+
+    document.querySelector('.theme-name').textContent = newTheme;
+}
+document.querySelector('.theme-toggle').addEventListener('click', setTheme);
