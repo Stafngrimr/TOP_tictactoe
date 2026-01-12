88fd89855b1c39a33008ba65676d54e893f5e0d6
Stephen Carder <stafngrimr@hotmail.co.uk>
2026-01-12 17:42:53 +0000

    actually started it

--- script.js
+++ script.js
@@ -6,59 +6,27 @@ Object to control the flow of the game
 NO GLOBAL CODE I SAID!.. not yet
 NO GLOBAL CODE.
 */
-function createUser (name) {
-    const printName = () => name[0].toUpperCase() + name.slice(1);
-    name = name[0].toUpperCase() + name.slice(1);
-    let level = 0;
-    const getLevel = () => level;
-    const levelUp = () => level++;
 
-    let reputation = 0;
-    const getRep = () => reputation;
-    const repUp = () => reputation++;
+const gameBoard = (function() {
+    const 0L = 0;
+    const 0M = 0;
+    const 0R = 0;
+    const 1L = 0;
+    const 1M = 0;
+    const 1R = 0;
+    const 2L = 0;
+    const 2M = 0;
+    const 2R = 0;
+})();
 
-    return { name, printName, getLevel, levelUp, getRep, repUp };
+
+function createPlayer(name, marker) {
+    return { name, marker }
 }
 
-function createPlayer (name, gameClass) {
-    const { printName, getLevel, levelUp, getRep, repUp } = createUser(name);
-
-    const discordName = "@" + name;
-    const weapon = "Mighty Axe"
-
-    return { name, printName, gameClass, getLevel, levelUp, getRep, repUp, discordName, weapon };
-}
-
-function createEnemy (name, type) {
-    const { printName, getLevel, levelUp } = createUser(name);
-
-    const weapon = "Festering Rod";
-
-    return { name, printName, type, getLevel, levelUp, weapon };
-}
-
-const stephen = createPlayer("stephen", "warrior");
-const goblin001 = createEnemy("horatio", "goblin");
-
-
-stephen.levelUp();
-stephen.levelUp();
-stephen.levelUp();
-
-console.log(`${stephen.printName()}'s level is ${stephen.getLevel()} and the gobbo's is a measly ${goblin001.getLevel()}.`);
-
-console.log(`Also, ${stephen.name} the ${stephen.gameClass} is holding the ${stephen.weapon}, whilst ${goblin001.name} the ${goblin001.type} is wielding the ${goblin001.weapon}`);
-
-
-goblin001.levelUp();
-goblin001.levelUp();
-goblin001.levelUp();
-stephen.repUp();
-stephen.repUp();
-stephen.repUp();
-stephen.repUp();
-stephen.repUp();
-stephen.repUp();
-
-console.log(goblin001.name);
-console.log(goblin001.printName());
+const game = (function() {
+    const play1 = createPlayer("Steve", "x");
+    const play2 = createPlayer("Tom", "o");
+    
+    console.log("Players " + play1.name + " and " + play2.name + " have entered the arena!");
+})();
